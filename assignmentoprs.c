3b72e184624f8722f1937f9c5d5e8e4860850510
ggvignesh <101515184+ggvignesh@users.noreply.github.com>
2022-03-28 10:11:56 +0530

    Add files via upload

---
+++ assignmentoprs.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a=4;
+	printf("a+=5 is %d", a+=5);
+	printf("\n a-=6 is %d", a-=6);
+	printf("\n a*=7 is %d", a*=7);
+	printf("\n a%=2 is %d", a%=2);
+	printf("\n a/=3 is %d", a/=3);
+}
