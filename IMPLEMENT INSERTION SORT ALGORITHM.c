ad202302dd651f26536a1c1722442c3a92e90df7
ggvignesh <101515184+ggvignesh@users.noreply.github.com>
2022-07-10 11:22:58 +0530

    Create IMPLEMENT INSERTION SORT ALGORITHM.c

---
+++ IMPLEMENT INSERTION SORT ALGORITHM.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void insertion(int arr[], int n)
+{
+    int i, j;
+    for (i = 1; i < n; i++) {
+        int key = arr[i];
+        j = i - 1;
+        while (key < arr[j] && j >= 0) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+    printf("\nAfter performing Insertion sort:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+}
+
+int main()
+{
+
+    int arr[] = { 10, 14, 3, 8, 5, 12, 16, 13 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertion(arr, n);
+
+    return 0;
+}
